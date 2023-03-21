5431ad54cad9947ac58e92c49b1c2c817b73905b
K. Canamar | Remote Full Stack Software Engineer <99228411+kcanamar@users.noreply.github.com>
2023-03-21 10:28:50 -0700

    adds gesture to emoji, doubling in size

--- App.js
+++ App.js
@@ -1,7 +1,7 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, View } from 'react-native';
 import { useState } from 'react'
-
+import { GestureHandlerRootView } from 'react-native-gesture-handler'
 import Button from "./components/Button";
 import ImageViewer from './components/ImageViewer';
 import * as ImagePicker from 'expo-image-picker'
@@ -52,30 +52,32 @@ export default function App() {
 
 
   return (
-    <View style={styles.container}>
-      <View style={styles.imageContainer}> 
-        <ImageViewer placeholderImageSource={PlaceholderImage} selectedImage={selectedImage}/>
-        {pickedEmoji !== null ? <EmojiSticker imageSize={40} stickerSource={pickedEmoji} /> : null}
-      </View>
-      {showAppOptions ? (
-        <View style={styles.optionsContainer}>
-          <View style={styles.optionsRow}>
-            <IconButton icon='refresh' label='Reset' onPress={onReset}/>
-            <CircleButton onPress={onAddSticker}/>
-            <IconButton icon='save-alt' label='Save' onPress={onSaveImageAsync}/>
+    <GestureHandlerRootView style={styles.container}>
+      <View style={styles.container}>
+        <View style={styles.imageContainer}>
+          <ImageViewer placeholderImageSource={PlaceholderImage} selectedImage={selectedImage}/>
+          {pickedEmoji !== null ? <EmojiSticker imageSize={40} stickerSource={pickedEmoji} /> : null}
+        </View>
+        {showAppOptions ? (
+          <View style={styles.optionsContainer}>
+            <View style={styles.optionsRow}>
+              <IconButton icon='refresh' label='Reset' onPress={onReset}/>
+              <CircleButton onPress={onAddSticker}/>
+              <IconButton icon='save-alt' label='Save' onPress={onSaveImageAsync}/>
+            </View>
           </View>
-        </View>
-        ) : (
-        <View style={styles.footerContainer}>
-          <Button theme="primary" label="Choose a photo" onPress={pickImageAsync}/>
-          <Button label="Use this photo" onPress={() => setShowAppOptions(true)}/>
-        </View>
-      )}
-      <EmojiPicker isVisible={isModalVisable} onClose={onModalclose}>
-          <EmojiList onSelect={setPickedEmoji} onCloseModal={onModalclose} />
-      </EmojiPicker>
-      <StatusBar style="auto" />
-    </View>
+          ) : (
+          <View style={styles.footerContainer}>
+            <Button theme="primary" label="Choose a photo" onPress={pickImageAsync}/>
+            <Button label="Use this photo" onPress={() => setShowAppOptions(true)}/>
+          </View>
+        )}
+        <EmojiPicker isVisible={isModalVisable} onClose={onModalclose}>
+            <EmojiList onSelect={setPickedEmoji} onCloseModal={onModalclose} />
+        </EmojiPicker>
+        <StatusBar style="auto" />
+      </View>
+    </GestureHandlerRootView>
   );
 }
 
--- components/EmojiSticker.js
+++ components/EmojiSticker.js
@@ -1,13 +1,46 @@
 import { View, Image } from 'react-native'
+import { TapGestureHandler } from 'react-native-gesture-handler'
+import Animated, {
+    useAnimatedStyle,
+    useSharedValue,
+    useAnimatedGestureHandler,
+    withSpring,
+} from 'react-native-reanimated'
+
+// https://github.com/software-mansion/react-native-reanimated/issues/3355
+window._frameTimestamp = null
+
+const AnimatedImage = Animated.createAnimatedComponent(Image)
 
 export default function EmojiSticker({ imageSize, stickerSource }) {
+
+    const scaleImage = useSharedValue(imageSize)
+
+    const onDoubleTap = useAnimatedGestureHandler({
+        onActive: (event) => {
+            console.log({event})
+            if (scaleImage.value) {
+                scaleImage.value = scaleImage.value * 2
+            }
+        }
+    })
+
+    const imageStyle = useAnimatedStyle(() => {
+        return {
+            width: withSpring(scaleImage.value),
+            height: withSpring(scaleImage.value),
+        }
+    })
+
     return (
         <View style={{ top: -350 }}>
-            <Image
-                source={stickerSource}
-                resizeMode='contain'
-                style={{ width: imageSize, height: imageSize }}
-            />
+            <TapGestureHandler onGestureEvent={onDoubleTap} numberOfTaps={2}>
+                <AnimatedImage
+                    source={stickerSource}
+                    resizeMode='contain'
+                    style={[imageStyle, { width: imageSize, height: imageSize }]}
+                />
+            </TapGestureHandler>
         </View>
     )
 }
