5e7ea29e17a83bf526e814fa936d519d0fb60acb
K. Canamar | Remote Full Stack Software Engineer <99228411+kcanamar@users.noreply.github.com>
2023-03-22 12:20:05 -0700

    handle platform differences

--- App.js
+++ App.js
@@ -1,5 +1,5 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, View } from 'react-native';
+import { StyleSheet, View, Platform } from 'react-native';
 import { useRef, useState } from 'react'
 import { GestureHandlerRootView } from 'react-native-gesture-handler'
 import Button from "./components/Button";
@@ -12,6 +12,7 @@ import EmojiList from './components/EmojiList';
 import EmojiSticker from './components/EmojiSticker';
 import * as MediaLibrary from 'expo-media-library';
 import { captureRef } from 'react-native-view-shot';
+import domtoimage from 'dom-to-image';
 
 const PlaceholderImage = require("./assets/images/background-image.png")
 
@@ -55,20 +56,38 @@ export default function App() {
   }
 
   const onSaveImageAsync = async () => {
-    try {
-      const localUri = await captureRef(imageRef, {
-        height: 440,
-        quality: 1,
-      })
+    if ( Platform.OS !== 'web') {
+      try {
+        const localUri = await captureRef(imageRef, {
+          height: 440,
+          quality: 1,
+        })
 
-      await MediaLibrary.saveToLibraryAsync(localUri);
+        await MediaLibrary.saveToLibraryAsync(localUri);
 
-      if(localUri) {
-        alert("Saved!")
+        if(localUri) {
+          alert("Saved!")
+        }
+
+      } catch (error) {
+        console.log(error)
       }
-
-    } catch (error) {
-      console.log(error)
+    } else {
+      domtoimage
+        .toJpeg(imageRef.current, {
+          quality: 0.95,
+          width: 320,
+          height: 440,
+        })
+        .then((dataUrl) => {
+          let link = document.createElement('a');
+          link.download = 'first-app.jpeg';
+          link.href = dataUrl;
+          link.click()
+        })
+        .catch((error) => {
+          console.log(error)
+        })
     }
   }
 
